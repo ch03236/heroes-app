64596c4ec2d0155e91fa0cb9a12f13f4b764129a
chbv <christian@girasolo.com>
2019-02-21 16:33:44 -0600

    Inicializacion de la aplicación

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//Rutas
+import { AppRoutingModule } from './app-routing.module';
+import {APP_ROUTING} from './app.routes'
+
+//Componetes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
