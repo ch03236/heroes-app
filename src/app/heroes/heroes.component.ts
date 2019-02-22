f921296b00dcd09f9e3aab1c31649eb193f6173d
chbv <christian@girasolo.com>
2019-02-22 09:04:16 -0600

    Consumiendo el servicio de heroes desde el heroes.component.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import  {HeroesService} from '../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  providers:[HeroesService]
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[]= [];
 
-  ngOnInit() {
+  constructor(private _heroesServicio: HeroesService) { 
+    console.log("Contructor")
   }
 
+  ngOnInit() {
+    this.heroes = this._heroesServicio.getHeroes();
+    console.log(this.heroes)
+  }
+
+}
+
+export interface Heroe{
+  npmbre: string;
+  bio: string;
+  img: string;
+  aparicion: string;
+  casa: string;
 }
